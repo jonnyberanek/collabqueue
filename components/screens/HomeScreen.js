2827dcfe9fc1f7fba5d901c1d6b145fb99857493
Jonny <jonnyberanek@gmail.com>
2019-06-06 03:19:07 -0400

    very basic party creation and matching

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -5,6 +5,9 @@ import { View, Button } from 'react-native'
 
 import Screen from 'components/abstracts/Screen'
 
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+
 const styles = {
   screen: {
       flex: 1,
@@ -17,17 +20,54 @@ const styles = {
   }
 }
 
+
+
 class HomeScreen extends Screen {
   static navigationOptions = {
     title: "Welcome"
   };
+
+  _createParty = async () => {
+    const db = firebase.firestore();
+
+    const makeid = () => {
+      var res = ""
+      const pc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+      for(var i = 0; i < 6; i++){ res += pc[Math.floor(Math.random()*pc.length)]}
+      return res
+    }
+
+    var possibleCode = makeid();
+
+    const parties = await db.collection('parties').get();
+
+    const usedCodes = parties.docs.map((doc) => doc.data().code)
+    while(usedCodes.some(c => c == possibleCode)){
+      possibleCode = makeid();
+    }
+
+    const createdParty = await db.collection('parties').add({
+      code: possibleCode,
+      host_id: "xyz",
+    })
+
+    if(createdParty){
+      this.props.navigation.navigate("HostDashboard", { code: possibleCode })
+    }
+    else{
+      // TODO: notify user of error
+    }
+
+
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style = {styles.screen}>
         <Button
           title="Start a Party!"
-          onPress={() => navigate("HostDashboard", { code: "01234" })}
+          onPress={this._createParty}
         />
         <Button
           title="Join a Party!"
